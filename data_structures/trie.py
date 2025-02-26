d686fa58603c9cc7d1ace4f69cd9aaced333e8ef
pranay0205 <pranayghuge@gmail.com>
2025-02-25 21:44:45 -0500

    add search level functionality to trie

--- data_structures/trie.py
+++ data_structures/trie.py
@@ -1,4 +1,28 @@
 class Trie:
+    def search_level(self, current_level, current_prefix, words):
+        if self.end_symbol in current_level:
+            words.append(current_prefix)
+
+        current_keys = sorted(current_level.keys())
+        print(f"current keys : {current_keys}")
+        for key in current_keys:
+            if key != self.end_symbol:
+                words = self.search_level(
+                    current_level[key], current_prefix + key, words)
+
+        return words
+
+    def words_with_prefix(self, prefix):
+        matching_words = []
+        current_level = self.root
+        for letter in prefix:
+            if letter not in current_level:
+                return []
+            current_level = current_level[letter]
+
+        matching_words = self.search_level(current_level, prefix, [])
+        return matching_words
+
     def exists(self, word):
         current = self.root
         for letter in word:
